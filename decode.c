4d2d069e1290d3d5b5edad25a0a2a84bb97ec032
Benjamin Medicke <bmedicke@gmail.com>
2017-09-26 13:44:14 +0200

    add comments, clean up

--- decode.c
+++ decode.c
@@ -1,32 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdint.h> // 16 bit int type.
+#include <stdint.h> // for 16bit uint.
 
-#define CHUNK 16
+#define CHUNKSIZE 16
 #define FILENAME "in"
 
-int main(int argc, char *argv[])
-{
-  char buffer[CHUNK];
-  uint16_t coded_num;
-  FILE *file;
+int main(int argc, char *argv[]) {
+  char buffer[CHUNKSIZE];
+  uint16_t encoded_char;
+  FILE* file;
 
-  file = fopen(FILENAME, "r");
-  if (file)
-  {
-    for (int i = 0; i < 50; i++)
-    {
+  const char* filemode = "r";
+  file = fopen(FILENAME, filemode);
+
+  if (file) {
+    for (int i = 0; i < 50; i++) {
       fread(buffer, 1, sizeof buffer, file);
-      coded_num = strtoul(buffer, NULL, 2);
+      encoded_char = strtoul(buffer, NULL, 2);
 
-      // 65 A - 90 Z + 32 space:
-      for (int cipher = 1; cipher < 512; cipher *= 2){
-        int test_case = coded_num / cipher;
+      // try decoding with cipher 2^0 to 2^8:
+      for (int cipher = 1; cipher <= 256; cipher *= 2){
+        int test_case = encoded_char / cipher;
+
+        // expecting ASCII: 65(A) - 90(Z) + 32(space):
         if ((test_case <= 90 && test_case >= 65) || test_case == 32){
           printf("%.16s\t%i\t%c\t%i\n",
-              buffer, coded_num/cipher,
-              (char)(coded_num/cipher), cipher);
-          break;
+              buffer, encoded_char/cipher,
+              (char)(encoded_char/cipher), cipher);
+          break; // cipher found, skipping to next chunk.
         }
       }
     }
