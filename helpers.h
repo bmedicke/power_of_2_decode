f5915276071fb15967419afa2e63a882c54f4b4a
Benjamin Medicke <bmedicke@gmail.com>
2017-10-06 13:00:35 +0200

    fix verb forms

--- helpers.h
+++ helpers.h
@@ -18,12 +18,12 @@
 /** @brief inform user about proper usage of the program.  */
 void print_manual(void);
 
-/** @brief reads encoded and writes decoded text to a file */
+/** @brief read encoded text and write decoded text to a file */
 _Bool write_decoded_text(FILE *encoded_fd, FILE *decoded_fd, _Bool verbose);
 // TODO: make encoded_fd const?
 
 
-/** @brief read decoded text and writes statistics to a file */
+/** @brief read decoded text and write statistics to a file */
 _Bool write_statistics(FILE *decoded_fd, FILE *statistic_fd, _Bool verbose);
 // TODO: make decoded_fd const?
 
